ac829e427f5f1b57e9d1f9171d9330a2a679ce7c
bestfarmer94 <sjasjaruddus@naver.com>
2022-08-30 22:15:02 +0900

    Implement6
    class point {
    	int x;
    	int y;
    	public point(int x, int y) {
    		this.x = x;
    		this.y = y;
    	}
    }
    
    static ArrayList<point> home;
    home.add(new point(i, j));
    
    재귀 함수의 포인트는 for문의 숫자를 조절할 수 있다는 것에 있는 것 같다.
    익숙해 져야 한다.
    for(int i=start; i<chicken.size(); i++) {
    			chicken_open[i] = true;
    			min_distance(start + 1, count + 1);	// 재귀 함수를 통해 다중 for문이 만들어 진다. 이후에 위의 count == m과 return을 통해
    			chicken_open[i] = false;			// 그 이상의 생성은 막히게 된다.

--- src/Implement/Q13.java
+++ src/Implement/Q13.java
@@ -3,13 +3,31 @@ package Implement;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.StringTokenizer;
 
-import javax.swing.plaf.basic.BasicInternalFrameTitlePane.MaximizeAction;
+class point {
+	
+	int x;
+	int y;
+	
+	public point(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+}
 
 public class Q13 {
-
+	
+	// 실패했다.
+//	static int[][] home;
+//	static int[][] chicken;
+	static ArrayList<point> home;
+    static ArrayList<point> chicken;
+	static int m;
+	static int min;
+	static boolean[] chicken_open;
+	
 	public static void main(String[] args) throws IOException {
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -17,61 +35,71 @@ public class Q13 {
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
 		int n = Integer.parseInt(st.nextToken());
-		int m = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
 		
-		int[][] arr = new int[n][n];
-		int[][] home = new int[2][];
-		int[][] chicken = new int[2][];
-		
-		int count = 0;
-		int count2 = 0;
+//		home = new int[2][];
+//		chicken = new int[2][];
+		home = new ArrayList<>();
+		chicken = new ArrayList<>();
 		
 		for(int i=0; i<n; i++) {
 			st = new StringTokenizer(br.readLine());
 			
 			for(int j=0; j<n; j++) {
 				int number = Integer.parseInt(st.nextToken());
-				arr[i][j] = number;
 				if(number == 1) {
-					home[0][count] = i;
-					home[1][count] = j;
-					count++;
+					// 이게 안되더라...
+//					home[0][count] = i;
+//					home[1][count] = j;
+//					count++;
+					home.add(new point(i, j));
 				}
 				if(number == 2) {
-					chicken[0][count2] = i;
-					chicken[1][count2] = j;					
-					count2++;
+//					chicken[0][count2] = i;
+//					chicken[1][count2] = j;					
+//					count2++;
+					chicken.add(new point(i, j));
 				}
 			}
 		}
 		
-		int result = 0;
-		if(count2 <= m) {
-			for(int i=0; i<count; i++) {
-				int distance = Integer.MAX_VALUE;
-				for(int j=0; j<count; j++) {
-					distance = Math.min(distance, Math.abs(home[0][i] - chicken[0][j]));
-				}
-				result += distance;
-			}
-		}else {
-			int[] have = new int[m];
-			int z = 0;
-			while(true) {
-				
-				
-			}
-		}
+		chicken_open = new boolean[chicken.size()];
+		min = Integer.MAX_VALUE;
 		
-		sb.append(result);
+		min_distance(0, 0);
+		sb.append(min);
 		System.out.println(sb);
 		br.close();
 	}
 	
-	static int min_distance() {
+	static void min_distance(int start, int count) {
 		
-		int min;
+		if(count == m) {
+			int result = 0;
+			
+			for(int i=0; i<home.size(); i++) {
+			
+				int distance = Integer.MAX_VALUE;
+				
+				for(int j=0; j<chicken.size(); j++) {
+					if(chicken_open[j]) {
+						distance = Math.min(distance, 
+							Math.abs(home.get(i).x - chicken.get(j).x) 
+							+ Math.abs(home.get(i).y - chicken.get(j).y));
+					}
+				}
+				
+				result += distance;
+			}
+			
+			min = Math.min(min, result);
+			return;		// count = m 에서 막히게 되는 이유
+		}
 		
-		return min;
+		for(int i=start; i<chicken.size(); i++) {
+			chicken_open[i] = true;
+			min_distance(start + 1, count + 1);	// 재귀 함수를 통해 다중 for문이 만들어 진다. 이후에 위의 count == m과 return을 통해
+			chicken_open[i] = false;			// 그 이상의 생성은 막히게 된다.
+		}
 	}
 }
---
+++ src/Implement/Q14.java
@@ -0,0 +1,104 @@
+package Implement;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class Q14 {
+
+	static int n;
+	static ArrayList<Integer> weak;
+	static ArrayList<Integer> dist;
+	static int answer;
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		n = Integer.parseInt(br.readLine());
+		
+		weak = new ArrayList<>();
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		while(st.hasMoreTokens()) {
+			weak.add(Integer.parseInt(st.nextToken()));
+		}
+		
+		dist = new ArrayList<>();
+		st = new StringTokenizer(br.readLine());
+		
+		while(st.hasMoreTokens()) {
+			dist.add(Integer.parseInt(st.nextToken()));
+		}
+		
+		int number = weak.size();
+		for(int i=0; i<number; i++) {	// 일자로 map을 2배로 늘리기.
+			weak.add(weak.get(i) + n);
+		}
+		
+		answer = dist.size() + 1;
+		permutation(new boolean[dist.size()], new int[dist.size()], 0);
+		if(answer == dist.size() + 1) {
+			answer = -1;
+		}
+		sb.append(answer);
+		System.out.println(sb);
+		br.close();
+	}
+	
+	
+	static void permutation(boolean[] visited, int[] res, int depth) {
+		
+		if(depth == dist.size() || answer == 1) {
+			for(int i=0; i<res.length; i++) {
+				System.out.print(res[i]);
+			}
+			System.out.println();
+			check(res);
+			return;
+		}
+		
+		for(int i=0; i<dist.size(); i++) {
+			if(!visited[i]) {
+				res[depth] = dist.get(i);
+				visited[i] = true;
+				permutation(visited, res, depth + 1);
+				visited[i] = false;
+			}
+		}
+	}
+	
+	
+	static void check(int[] res) {
+		
+		for(int i=0; i<weak.size()/2; i++) {
+			int j = i;
+			int count = 0;
+			while(weak.get(j) < weak.get(i) + n) {
+				int position = weak.get(j) + res[count];
+				j++;
+				count++;
+				
+				if(position > weak.get(i) + n) {
+					break;
+				}
+				
+				while(position >= weak.get(j)) {
+					if(j < weak.size()-1) {
+						j++;
+					}else {
+						break;
+					}
+				}
+				
+				if(count == res.length) {
+					break;
+				}
+			}
+			answer = Math.min(answer, count);
+		}
+	}
+}
