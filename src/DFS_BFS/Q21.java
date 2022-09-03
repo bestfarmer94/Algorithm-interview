c38164c44f9ef61a8e667bebd498c65ab718c87d
bestfarmer94 <sjasjaruddus@naver.com>
2022-09-03 23:17:31 +0900

    DFS/BFS5+

--- src/DFS_BFS/Q21.java
+++ src/DFS_BFS/Q21.java
@@ -12,7 +12,7 @@ public class Q21 {
 	static int r;
 	
 	public static void main(String[] args) throws IOException {
-	
+		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 		StringTokenizer st = new StringTokenizer(br.readLine());
@@ -31,13 +31,24 @@ public class Q21 {
 			}
 		}
 		
-		move(0, 0);
+		int group = 0;
+		
+		for(int i=0; i<n+1; i++) {
+			for(int j=0; j<n+1; j++) {
+				move(i, j, group, 0);
+				group++;
+			}
+		}
 		
 		System.out.println(sb);
 		br.close();
 	}
 	
-	static void move(int group, int count) {
+	static void move(int x, int y, int group, int count) {
+		
+		if(x>=0 && x<n+1 && y>=0 && y<n+1) {
+			
+		}
 		
 		for(int i=0; i<n+1; i++) {
 			
